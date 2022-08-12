7d4912bd77e6603bc3a0a496dead914adb135d08
Daniel Iñigo <daniel.inigo@datatonic.com>
2022-08-12 12:39:27 +0000

    chore: add revenue analysis file and update the employees seed

--- dbt_bigquery_training/models/marts/core/dim_customers.sql
+++ dbt_bigquery_training/models/marts/core/dim_customers.sql
@@ -2,11 +2,12 @@ with
     customers as (
         select * from {{ ref('stg_customers') }}
     ),
-
     orders as (
         select * from {{ ref('stg_orders') }}
     ),
-
+    employes as (
+        select * from {{ ref('stg_employees') }}
+    ),
     customer_orders as (
         select
             customer_id,
@@ -26,11 +27,13 @@ with
             customers.last_name,
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
+            employes.employee_id is not null as is_employee,
             customer_orders.lifetime_value,
             coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
         from customers
         left join customer_orders using (customer_id)
+        left join employes using (customer_id)
     )
 
 select * from final
